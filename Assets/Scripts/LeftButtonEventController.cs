77e1b35f9f18876b3fb04a60bc4821e904c3351e
gerain <1461009741@qq.com>
2019-12-25 11:43:58 +0800

    all

---
+++ Assets/Scripts/LeftButtonEventController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class LeftButtonEventController : MonoBehaviour,IVirtualButtonEventHandler
+{
+    public GameObject vb;
+    public GameObject cha;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VirtualButtonBehaviour vbb  = vb.GetComponent<VirtualButtonBehaviour>();
+        if(vbb){
+            vbb.RegisterEventHandler(this);
+        }
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+    public void OnButtonPressed(VirtualButtonBehaviour vb){
+        Animator ani = cha.GetComponent<Animator>();
+        ani.SetTrigger("Left");
+        Debug.Log("左边按钮按下");
+    }
+    
+    public void OnButtonReleased(VirtualButtonBehaviour vb){
+        
+        Debug.Log("左边按钮释放");
+    }
+}
---
+++ Assets/Scripts/RightButtonEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class RightButtonEventHandler : MonoBehaviour,IVirtualButtonEventHandler
+{
+    public GameObject vb;
+    public GameObject cha;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        VirtualButtonBehaviour vbb  = vb.GetComponent<VirtualButtonBehaviour>();
+        if(vbb){
+            vbb.RegisterEventHandler(this);
+        }
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+    public void OnButtonPressed(VirtualButtonBehaviour vb){
+        Animator ani = cha.GetComponent<Animator>();
+        ani.SetTrigger("Right");
+        Debug.Log("右边按钮按下");
+    }
+    
+    public void OnButtonReleased(VirtualButtonBehaviour vb){
+        
+        Debug.Log("右边按钮释放");
+    }
+}
